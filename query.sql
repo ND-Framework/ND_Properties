60cb7547990f5b93e17080a4db52b269eaa39bae
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2022-12-01 23:09:55 +0100

    fix: address not in table

--- query.sql
+++ query.sql
@@ -1,8 +1,9 @@
 CREATE TABLE `nd_properties` (
-	`id` VARCHAR(250) NULL DEFAULT NULL,
-	`owner` INT(11) NULL DEFAULT NULL,
-	`access` LONGTEXT NULL DEFAULT '[]',
-	`sale` INT(11) NULL DEFAULT NULL,
-	INDEX `owner` (`owner`) USING BTREE,
-	CONSTRAINT `character` FOREIGN KEY (`owner`) REFERENCES `characters` (`character_id`) ON UPDATE CASCADE ON DELETE CASCADE
+    `id` VARCHAR(250) DEFAULT NULL,
+    `owner` INT(11) DEFAULT NULL,
+    `access` LONGTEXT DEFAULT '[]',
+    `address` VARCHAR(250) DEFAULT NULL,
+    `sale` INT(11) DEFAULT NULL,
+    INDEX `owner` (`owner`) USING BTREE,
+    CONSTRAINT `property_owner` FOREIGN KEY (`owner`) REFERENCES `characters` (`character_id`) ON UPDATE CASCADE ON DELETE CASCADE
 );
